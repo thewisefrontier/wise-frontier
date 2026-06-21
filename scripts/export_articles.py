f5672c218d56c6840e4ae034fa8dbb4ec5b9be8f
thewisefrontier <thewisefrontier@gmail.com>
2026-06-22 07:40:52 +0900

    Add live.html to sitemap

--- scripts/export_articles.py
+++ scripts/export_articles.py
@@ -182,6 +182,7 @@ def generate_sitemap(articles):
         '<url><loc>https://newsfinal.co.kr/privacy.html</loc><changefreq>monthly</changefreq><priority>0.3</priority></url>',
         '<url><loc>https://newsfinal.co.kr/terms.html</loc><changefreq>monthly</changefreq><priority>0.3</priority></url>',
         '<url><loc>https://newsfinal.co.kr/archive.html</loc><changefreq>daily</changefreq><priority>0.6</priority></url>',
+        '<url><loc>https://newsfinal.co.kr/live.html</loc><changefreq>hourly</changefreq><priority>0.8</priority></url>',
         '<url><loc>https://newsfinal.co.kr/calendar.html</loc><changefreq>daily</changefreq><priority>0.6</priority></url>',
         '<url><loc>https://newsfinal.co.kr/markets.html</loc><changefreq>hourly</changefreq><priority>0.7</priority></url>',
         '<url><loc>https://newsfinal.co.kr/country.html</loc><changefreq>daily</changefreq><priority>0.7</priority></url>',
