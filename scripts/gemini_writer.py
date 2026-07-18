cd15632af97a5352aee3eee84409449b1796b257
thewisefrontier <thewisefrontier@gmail.com>
2026-07-19 02:03:28 +0900

    업데이트

--- scripts/gemini_writer.py
+++ scripts/gemini_writer.py
@@ -88,6 +88,7 @@ GEMINI_API_KEYS = [k for k in [
     os.getenv("GEMINI_API_KEY_2"),
     os.getenv("GEMINI_API_KEY_3"),
     os.getenv("GEMINI_API_KEY_4"),
+    os.getenv("GEMINI_API_KEY_5"),
 ] if k]
 
 _current_key_idx = 0
